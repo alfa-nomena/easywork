19dd736d78d20b24283d2255dd111eb07d65a041
Nomena Alfa ANDRIAMIFIDISOA <perf.alfa@mmv.madamvalue.mg>
2023-04-06 12:41:16 +0300

    Editing templates listing jobs. Redirecting empty url to the jobs/home

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name="jobs"
 urlpatterns = [
-    path("", views.home, name='home'),
-    path('all/', views.ListAllJobsView.as_view(), name="all_jobs")
+    path("home/", views.home, name='home'),
+    path('all/', views.ListAllJobsView.as_view(), name="list_all_jobs")
 ]
